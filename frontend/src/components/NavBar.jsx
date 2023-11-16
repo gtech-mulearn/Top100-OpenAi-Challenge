7978f80a4e8146453ba3711a388ada0d403b9ab2
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-11-16 23:54:25 +0530

    home page created

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  const [active, setActive] = useState(false);
+  const [cls, setCls] = useState("inactive");
+
+  function openNav() {
+    setActive(true);
+    setCls("Active");
+  }
+  function closeNav() {
+    setActive(false);
+    setCls("inactive");
+  }
+  return (
+    <nav>
+      <a href="/">
+        <img src="/images/logo.jpg" alt="" />
+      </a>
+      {/* <ul style={{ width: `${width}` }}> */}
+      <ul className={cls}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/squad">Reports</NavLink>
+        </li>
+        <li>
+          <NavLink to="/photos">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">Contact</NavLink>
+        </li>
+      </ul>
+      {active ? (
+        <button className="menu-container" onClick={closeNav}>
+          <CloseIcon className="menu close" />
+        </button>
+      ) : (
+        <button className="menu-container" onClick={openNav}>
+          <MenuIcon className="menu" />
+        </button>
+      )}
+    </nav>
+  );
+}
+export default NavBar;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import NavBar from '../components/NavBar'
 function Home() {
   return (
-    <div>
-      hello
-    </div>
-  )
+    <main>
+      <NavBar />
+      <section className="hero">
+  
+        <h1>Med reports</h1>
+          <button>Get report</button>
+      </section>
+    </main>
+  );
 }
 
 export default Home
