63590d5764806430238264033d969cb4c1b1d380
anjali k <anjali.k@fayausa.com>
2023-11-17 15:49:34 +0530

    feat(resume_analyzer):add code

--- skillspotter/resume_analyzer/views.py
+++ skillspotter/resume_analyzer/views.py
@@ -1,9 +1,73 @@
-# myapp/views.py
+import re
 from django.shortcuts import render
 from django.http import JsonResponse
 import openai
 import fitz
 from .forms import ResumeUploadForm
+from .main import PdfParser
+from .github import GitHub 
+import copy
+import os
+
+def extract_links(request,text):
+    # Use a regular expression to find URLs in the text
+    summarized_resume=copy.deepcopy(text)
+    url_pattern = re.compile(r'https?://github\S+')
+    links = re.findall(url_pattern, text) 
+    print(links)
+    if len(links)>1:
+        github=GitHub(links[0])
+    else:
+        return None
+        response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=generate_resume_summarization_task(summarized_resume),
+                max_tokens=500,
+            )
+        ai_summary = response.choices[0].text.strip()
+        # return JsonResponse({'task_generated': task_generated})
+        return render(request, 'summary.html', {'ai_summary': ai_summary})
+    print("result:",github.results)
+    return github.results
+
+def generate_resume_summarization_task(resume_summary):
+    """
+    Generate a resume summarization machine task for testing candidate capabilities.
+
+    Args:
+    - resume summarized summary
+    Returns:
+    - str: The generated task with placeholders.
+    """
+
+    task_template = f"""
+    **Task: Machine Test**
+
+    ---
+
+    **Description:**
+
+    Recruiters often need to give some task to verify candidate knowledge
+
+    **Input:**
+
+    Provide a resume summary generated using openai.
+
+    **Output:**
+
+    The system should generate task according to candidate skills:
+
+    
+
+    **Example Input:**
+
+    ```
+    {resume_summary}
+   
+    """
+
+    return task_template.strip()
+
 
 def analyze_resume(request):
     form = ResumeUploadForm()
@@ -13,6 +77,7 @@ def analyze_resume(request):
         if form.is_valid():
             # Get the uploaded resume PDF file
             resume_pdf = form.cleaned_data['resume_pdf']
+            
 
             # Read the resume PDF and extract text
             doc = fitz.open(resume_pdf)
@@ -23,17 +88,35 @@ def analyze_resume(request):
             job_description = form.cleaned_data['job_description']
 
             # Call OpenAI API for summarization
-            openai.api_key = ""
+            openai.api_key = os.environ.get("OPENAI_API_KEY")
             response = openai.Completion.create(
                 engine="text-davinci-003",
-                prompt=f"\n Conduct a thorough assessment of the candidate's profile by examining their educational background, work history, skills, and noteworthy accomplishments. Evaluate the alignment of their experiences with career goals, considering aspects such as career progression, exceptional strengths, and potential areas of concern. Additionally, provide a summary of the candidate's potential for growth and adaptability in diverse contexts. In the context of a specific job description, rigorously analyze the candidate's suitability for the specified position. Start by assessing if the candidate meets the essential qualifications outlined in the job description. Subsequently, offer a detailed and impartial assessment of their professional background, skills, and experiences, emphasizing relevant achievements. Maintain objectivity, avoiding overemphasis or bias, while presenting an accurate depiction of the candidate's qualifications and potential fit for the role. Highlight key strengths and areas of expertise, giving due consideration to potential gaps or areas for improvement. Furthermore, provide insights into how the candidate could contribute to the organization, ensuring a nuanced and objective evaluation. Additionally, include links to relevant platforms such as GitHub and LinkedIn from the provided resume to facilitate a more in-depth view of their professional portfolio in bulletin format.{job_description}\n{resume_text}",
+                prompt=f"\n Conduct a comprehensive analysis of the individual’s profile, taking into account all the information provided. This should include an evaluation of their educational background, work history, skills, and notable achievements. Be sure to also examine any links included in the resume for additional information, especially those leading to professional networking sites like LinkedIn, or repositories showcasing their work, such as GitHub. Identify any potential areas of concern or exceptional strengths. Consider their career progression and how their experiences align with their goals. Also, assess their potential for growth and adaptability in various contexts. Remember to scrape all the links in the resume, particularly those to LinkedIn, GitHub, etc., for a thorough understanding of the individual’s profile,please list links like github and linkedin.{job_description}\n{resume_text}",
                 max_tokens=500,
             )
 
             # Extract the summary from OpenAI's response
             summary = response.choices[0].text.strip()
-            request.summary = summary
+            request.summary = copy.deepcopy(summary)
 
-            return JsonResponse({'summary': summary})
+            git_repo_summary = extract_links(request,summary)
 
+            resume_summary=request.summary
+     
+
+            # Make an API call to OpenAI GPT-3
+            response2 = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=f"\n Given a candidate's GitHub profile JSON data and summarized resume, compare the two sources and provide tailored insights for recruiters. Highlight any discrepancies, showcase complementary information,and offer insights that recruiters can leverage to make informed decisions. Consider factors such as the candidate's skills, academic background, versatility, achievements, GitHub activity, and any potential for growth. Ensure the insights are presented in a way that facilitates recruiters in assessing the candidate's overall suitability for the position{resume_summary}\n{git_repo_summary} ",
+                max_tokens=200,  # Adjust max_tokens as needed
+            )
+
+            # Extract the overall summary from the response
+            ai_summary = response2.choices[0].text.strip()
+
+            # request.summary = summary
+            # return render(request,'summary.html', {"ai_summary",ai_summary})
+            # return JsonResponse({'summary': ai_summary})
+            return render(request, 'summary.html', {'ai_summary': ai_summary})
+git 
     return render(request, 'upload_pdf.html', {'form': form})
