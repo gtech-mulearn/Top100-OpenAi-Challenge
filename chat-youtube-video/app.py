1d486be4b9c8c1c1573012d31edb560dedb1a6d3
Abhishek S Lal <53397244+Abhishek-S-Lal@users.noreply.github.com>
2023-11-17 12:18:56 +0530

    file uploader added

--- chat-youtube-video/app.py
+++ chat-youtube-video/app.py
@@ -1,33 +1,47 @@
 import streamlit as st
 from businessLogic import transcribeVideoOrchestrator
+from businessLogic import transcribeVideoCustom
+
 
 def main():
     st.set_page_config(page_title="Chat Youtube", page_icon="🏖", layout="centered")
-
+    MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
     
-    st.title("Chat Youtube")    
-    st.write("Seamlessly converse with YouTube videos, receive accurate text responses, and skip the lengthy video content.")    
+    st.title("Chat with Video")    
+    st.write("Seamlessly converse with videos, receive text responses, and skip watching the lengthy video content.")    
 
-  
-    # st.write("##")
+
     # User input: YouTube URL
     url = st.text_input("Enter YouTube URL:")    
+    my_upload = st.file_uploader("Upload a video", type=["mp4"])
+    print(my_upload)
 
     # User input: model
     # models = ["tiny", "base", "small", "medium", "large"]
     # model = st.selectbox("Select Model:", models)
     # st.write(
     #     "If you take a smaller model it is faster but not as accurate, whereas a larger model is slower but more accurate.")
-    if st.button("Transcribe"):
-        if url:
-            transcript = transcribeVideoOrchestrator(url, "tiny")
-
-            if transcript:
-                st.subheader("Transcription:")
-                st.write(transcript)
+    if st.button("Process Video"):
+        if my_upload is not None:
+            if my_upload.size > MAX_FILE_SIZE:
+                st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
             else:
-                st.error("Error occurred while transcribing.")
-                st.write("Please try again.")
+                print(my_upload)
+                # transcribeVideoCustom(my_upload, "whisper-1")
+        else:
+            if url:
+                result = transcribeVideoOrchestrator(url, "tiny")
+
+                if result:
+                    st.subheader("Transcription:")
+                    st.write(result.get('transcription'))
+                    st.subheader("Points covered:")
+                    st.write(result["topics"])
+                    st.subheader("Content Analysis")
+                    st.write(result["analysis"])
+                else:
+                    st.error("Error occurred while transcribing.")
+                    st.write("Please try again.")
     
 
     st.markdown('<div style="margin-top: 450px;"</div>',
--- chat-youtube-video/businessLogic.py
+++ chat-youtube-video/businessLogic.py
@@ -1,18 +1,29 @@
 import tempfile
 from pprint import pprint
-
+from pydub import AudioSegment
 import whisper
 from pytube import YouTube
 from openai import OpenAI
+import io
 import os
+from typing import Union
+import streamlit as st
 
+client = OpenAI()
 
 def transcribeVideoOrchestrator(youtube_url: str,  model_name: str):
     # audio = downloadYoutubeVideo(youtube_url)
     audio = downloadYoutubeAudio(youtube_url)
     # transcription = transcribe(audio, model_name)
     transcription = transcribeAPI(audio, model_name)
-    return transcription
+    topics = discoverTopics(transcription)
+    analysis = analyseTopics(topics)
+    result = {
+        "transcription": transcription,
+        "topics": topics,
+        "analysis": analysis
+    }
+    return result
 
 
 def transcribe(audio: dict, model_name="medium"):
@@ -26,10 +37,7 @@ def transcribe(audio: dict, model_name="medium"):
 def transcribeAPI(audio: dict, model_name="whisper-1"):
     print("Transcribing...", audio['name'])
     print("Using model:", model_name)
-    client = OpenAI(
-        api_key=os.environ.get("OPENAI_API_KEY"),
-    )
-    print(os.environ.get("OPENAI_API_KEY"))
+    print(client.api_key)
     audio_file= open(audio['path'], "rb")
     result = client.audio.transcriptions.create(
     model="whisper-1", 
@@ -37,7 +45,69 @@ def transcribeAPI(audio: dict, model_name="whisper-1"):
     response_format="text"
     )
     pprint(result)
-    return result["text"]
+    return result
+
+    # print("Transcribing...", audio_path)
+    print("Using model:", model_name)
+    
+    try:
+        if isinstance(audio, st.uploaded_file_manager.UploadedFile):
+            # If audio is an UploadedFile object from Streamlit
+            audio_path = save_uploaded_file(audio)
+        elif isinstance(audio, str):
+            # If audio is already a file path
+            audio_path = audio
+        else:
+            raise ValueError("Invalid audio input")
+
+        print("Transcribing...", audio_path)
+        print("Using model:", model_name)
+
+        with open(audio_path, "rb") as audio_file:
+            result = client.audio.transcriptions.create(
+                model=model_name,
+                file=audio_file,
+                response_format="text"
+            )
+            pprint(result)
+            return result
+
+
+    except Exception as e:
+        print(f"Error occurred during transcription: {str(e)}")
+        return None
+    
+
+
+def discoverTopics(text: str, model_name = "gpt-3.5-turbo"):
+    print("Points Summarizing...")
+    print("Using model:", model_name)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant designed to summarize text using bullet points and appropriate topics."},
+            {"role": "user", "content": text}
+            ],
+        temperature=0,
+        max_tokens=1024
+    )
+    pprint(response.choices[0].message.content)
+    return response.choices[0].message.content
+
+def analyseTopics(text: str, model_name = "gpt-3.5-turbo"):
+    print("Content Analysing...")
+    print("Using model:", model_name)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Analyse content for: Spam, deceptive practices and scams, Sensitive content, Child safety , Nudity and sexual content, Suicide and self-harm , Vulgar language, etc. If yes, give reason. If no, say what type of content the text has."},
+            {"role": "user", "content": text}
+            ],
+        temperature=0,
+        max_tokens=1024
+    )
+    pprint(response.choices[0].message.content)
+    return response.choices[0].message.content
 
 def downloadYoutubeVideo(youtube_url: str) -> dict:
     print("Processing : " + youtube_url)
