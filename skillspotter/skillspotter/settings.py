28f92aed920a5fc6ab82d2679bfc91607aeeae44
Arfaz Khan <47222474+arfazkhan@users.noreply.github.com>
2023-11-18 16:17:20 +0530

    Updated settings.py

--- skillspotter/skillspotter/settings.py
+++ skillspotter/skillspotter/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path 
 import os
 from dotenv import load_dotenv
+from decouple import config
 
 # Load environment variables from .env file
 load_dotenv()
@@ -20,7 +21,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+OPENAI_API_KEY = config('OPENAI_API_KEY')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
